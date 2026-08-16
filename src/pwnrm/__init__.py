1fd4e5b067f6a57c27fa6d532058035c75287ee8
Hoang Gia <mrshadowsoul365@gmail.com>
2026-08-16 22:56:05 +0700

    chore: Bump version to 1.0.2 and update distribution files

--- src/pwnrm/__init__.py
+++ src/pwnrm/__init__.py
@@ -5,7 +5,7 @@ A Python library and CLI tool for WinRM-based post-exploitation
 and Active Directory enumeration.
 """
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __author__  = "uziii2208"
 
 from .core import (
